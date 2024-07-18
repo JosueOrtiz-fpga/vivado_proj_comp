ae0765ae955ec79481dde49211364c191dc16d4f
Josue Ortiz <ortizjo137@gmail.com>
2024-07-18 06:25:03 -0400

    Finished initial Makefile that builds a vivado project #2

--- make/scripts/viv_build.tcl
+++ make/scripts/viv_build.tcl
@@ -4,8 +4,20 @@ set proj_name [lindex $argv 0]
 set outputDir ./
 
 create_project -force $proj_name $outputDir
-set_property BOARD_PART [get_boards cora-z7-07s] [current_project]
-
+set_property board_part digilentinc.com:cora-z7-07s:part0:1.1 [current_project]
+# import the hdl files
+set src_path "../../top/$proj_name/src/"
+add_files -fileset sources_1 [ glob $src_path/*.v $src_path/*.vhd $src_path/*.sv]
+# import the constraint files
+set xdc_path "../../top/$proj_name/xdc/"
+add_files -fileset constrs_1  [ glob $xdc_path/*.xdc]
+# update the compile order
+update_compile_order
+# launch the runs
+launch_runs synth_1
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
 
 
 
