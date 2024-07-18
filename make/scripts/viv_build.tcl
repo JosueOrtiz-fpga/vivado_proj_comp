ae87f42c451ccdebacb15551ae0d32e1aa24c0a2
Josue Ortiz <ortizjo137@gmail.com>
2024-07-18 06:43:13 -0400

    Added the capability to source design files stored as tcl scripts #2

--- make/scripts/viv_build.tcl
+++ make/scripts/viv_build.tcl
@@ -8,6 +8,11 @@ set_property board_part digilentinc.com:cora-z7-07s:part0:1.1 [current_project]
 # import the hdl files
 set src_path "../../top/$proj_name/src/"
 add_files -fileset sources_1 [ glob $src_path/*.v $src_path/*.vhd $src_path/*.sv]
+# recreate any block design .tcl scripts
+set bd_path "$src_path/bd/"
+foreach bd [glob $bd_path/*.tcl] {
+    source $bd
+}
 # import the constraint files
 set xdc_path "../../top/$proj_name/xdc/"
 add_files -fileset constrs_1  [ glob $xdc_path/*.xdc]
