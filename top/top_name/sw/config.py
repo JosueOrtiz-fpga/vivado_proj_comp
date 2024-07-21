ca19cdd90d8b0a250bc33a6c49fa2dc15a5b14a4
Josue Ortiz <ortizjo137@gmail.com>
2024-07-21 07:08:48 -0400

    Imported the Vitis build flow changes from the z7_echo_server example that buidls the app using the Vitis Python API.
    Reorganized the top/top_name/ directory into sw and hw divisions

---
+++ top/top_name/sw/config.py
@@ -0,0 +1,14 @@
+# name of platform to be created
+platform_name = ""
+# name of processing domain that will be targeted
+domain_name = ""
+# what type of OS will be run on target domain
+os_name = ""
+cpu_name = ""
+domain_name = ""
+# bsp library names
+bsp_libs = [""]
+# name of the app to be created
+app_name =  ""
+# name of the template (AMD example app) to import
+template_name =  ""
